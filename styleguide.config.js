b97d74024b873f9265ec8bc48ea233b029d532a7
sergej.rupainis@gmail.com <sergej.rupainis@gmail.com>
2019-01-08 14:45:35 +0200

    Adding documentation to npm

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,5 +2,5 @@ module.exports = {
   webpackConfig: require('./config/webpack.styleguide.config'),
   components: 'src/components/**/[A-Z]*.jsx',
   assetsDir: 'src/assets',
-  styleguideDir: 'styleguide',
+  styleguideDir: 'documentation',
 };
